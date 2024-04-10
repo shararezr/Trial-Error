fcfafc4b222b4ebd294fef0c96423561dbe59c56
Sharare Zolghadr <zr.sharare@gmail.com>
2024-04-10 07:26:37 +0200

    Create jobscript.sh

---
+++ jobscript.sh
@@ -0,0 +1,31 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q hpc
+### -- set the job Name -- 
+#BSUB -J test_DiffuRec
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 1
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=4GB]"
+### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
+#BSUB -M 5GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
+### -- set the email address -- 
+### Please uncomment the following line and put in your e-mail address,
+### if you want to receive e-mail notifications on a non-default address
+#BSUB -u zr.sharare@gmail.com
+### -- send notification at start -- 
+#BSUB -B 
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o Output_%J.out 
+#BSUB -e Output_%J.err 
+
+python3 main.py
+
