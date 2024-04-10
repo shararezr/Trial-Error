2cef1219563b5ac29cf674776437f5a33389d716
Sharare Zolghadr <zr.sharare@gmail.com>
2024-04-10 11:54:59 +0200

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -5,7 +5,7 @@
 ### -- set the job Name -- 
 #BSUB -J testDiffuRec
 ### -- ask for number of cores (default: 1) -- 
-#BSUB -n 2
+#BSUB -n 4
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot -- 
@@ -22,10 +22,16 @@
 #BSUB -B 
 ### -- send notification at completion -- 
 #BSUB -N 
-### -- Specify the output and error file. %J is the job-id -- 
-### -- -o and -e mean append, -oo and -eo mean overwrite -- 
-#BSUB -o Output_%J.out 
-#BSUB -e Output_%J.err 
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o gpu_%J.out
+#BSUB -e gpu_%J.err
+# -- end of LSF options --
 
+nvidia-smi
+# Load the cuda module
+module load cuda/11.6
+
+/appl/cuda/11.6.0/samples/bin/x86_64/linux/release/deviceQuery
 python3 main.py 
 
