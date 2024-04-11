7fc710124b246b5c54a02f0281420b7dcbc1a04c
Sharare Zolghadr <zr.sharare@gmail.com>
2024-04-11 11:41:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ default_params = {
     'emb_dropout': 0.3,
     'hidden_act': 'gelu',
     'num_blocks': 4,
-    'epochs': 5,
+    'epochs': 3,
     'decay_step': 100,
     'gamma': 0.1,
     'metric_ks': [5, 10, 20],
