76699a1ca6e5ff4fed549b051c0442847f2c09fa
Sharare Zolghadr <zr.sharare@gmail.com>
2024-04-26 12:22:01 +0200

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -5,7 +5,7 @@
 ### -- set the job Name -- 
 #BSUB -J testDiffuRec
 ### -- ask for number of cores (default: 1) -- 
-#BSUB -n 1
+#BSUB -n 4
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1"
 ### -- specify that the cores must be on the same host -- 
