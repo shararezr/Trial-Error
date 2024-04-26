69c580df4737d7152403b02db26ba00cb1a274a2
Sharare Zolghadr <zr.sharare@gmail.com>
2024-04-26 13:31:00 +0200

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -12,6 +12,7 @@
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot -- 
 #BSUB -R "select[gpu32gb]"
+#BSUB -R "rusage[mem=4GB]"
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
 ### -- set walltime limit: hh:mm -- 
 #BSUB -W 24:00 
