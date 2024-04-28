20d3c65bdaa76f903ce6c1342275022005fdd640
Sharare Zolghadr <zr.sharare@gmail.com>
2024-04-28 20:48:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ default_params = {
     'noise_schedule': 'trunc_lin',
     'rescale_timesteps': True,
     'eval_interval': 10,
-    'patience': 100,
+    'patience': 5,
     'description': 'Diffu_norm_score',
     'long_head': False,
     'diversity_measure': False,
