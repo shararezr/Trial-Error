e77a899ce129d0851502414c233a9a6913fdf19a
Sharare Zolghadr <zr.sharare@gmail.com>
2024-04-29 10:58:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ default_params = {
     'dataset': 'ml-1m',
     'log_file': 'log/',
     'random_seed': 1997,
-    'max_len': 100,
+    'max_len': 50,
     'device': 'cuda',
     'num_gpu': 2,
     'batch_size': 512,
