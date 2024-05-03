d58d0fccfe3e7ac6be12ad9836c6b4cf5d28ee12
Sharare Zolghadr <zr.sharare@gmail.com>
2024-05-03 14:05:26 +0200

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -7,11 +7,11 @@
 ### -- ask for number of cores (default: 1) -- 
 #BSUB -n 4
 ### -- Select the resources: 1 gpu in exclusive process mode --
-#BSUB -gpu "num=2"
+#BSUB -gpu "num=1"
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[mem=10GB]"
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
 ### -- set walltime limit: hh:mm -- 
 #BSUB -W 24:00 
