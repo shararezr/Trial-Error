d9ddd3dd33e95a77018ef385ef62ee9f68cbfa79
Sharare Zolghadr <zr.sharare@gmail.com>
2024-05-06 14:15:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ default_params = {
     'optimizer': 'Adam',
     'lr': 0.001,
     'loss_lambda': 0.001,
-    'weight_decay': 0,
+    'weight_decay': 0.0001,
     'momentum': None,
     'schedule_sampler_name': 'lossaware',
     'diffusion_steps': 32,
