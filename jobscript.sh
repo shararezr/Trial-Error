7c7fd527ecc8e066f0ce2141d802e4a3a31c1dac
Sharare Zolghadr <zr.sharare@gmail.com>
2024-05-07 06:53:09 +0200

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -5,13 +5,13 @@
 ### -- set the job Name -- 
 #BSUB -J testDiffuRec
 ### -- ask for number of cores (default: 1) -- 
-#BSUB -n 8
+#BSUB -n 4
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1"
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 4GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[mem=10GB]"
 #BSUB -R "select[gpu32gb]"
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
 ### -- set walltime limit: hh:mm -- 
