fb43996183757160ddea0977b434a976ef7e58f4
Sharare Zolghadr <zr.sharare@gmail.com>
2024-05-17 11:55:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import time
 default_params = {
     'dataset': 'ml-1m',
     'log_file': 'log/',
-    'random_seed': 1987,
+    'random_seed': 1967,
     'max_len': 100,
     'device': 'cuda',
     'num_gpu': 1,
