6ca949f0b5aacc2801afb94e4913b39f253cd3d2
Sharare Zolghadr <zr.sharare@gmail.com>
2024-06-13 19:11:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -380,7 +380,7 @@ def main(args):
     torch.cuda.empty_cache()
     
     fix_random_seed_as(args.random_seed)
-    path_data = 'dataset.pkl'
+    path_data = 'datasetT.pkl'
     with open(path_data, 'rb') as f:
         data_raw = pickle.load(f)
 
